e89d67bc2bd55723e4d9a65af8c00b42fc14469e
plwang-berkeley <plwang@berkeley.edu>
2021-04-27 11:57:21 -0700

    Added all files

---
+++ assets/script/script.js
@@ -0,0 +1,6 @@
+function makeDarkMode(color){
+    let cakeid = document.getElementById("title");
+    cakeid.style.backgroundColor = "#EF476F";
+}
+
+document.getElementById("title").onclick = makeDarkMode;
