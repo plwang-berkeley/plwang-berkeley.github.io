d1fe069c1f6babf529b6ebc718c8331d75910605
Michael Chang <22mchang@berkeley.edu>
2021-04-27 17:42:10 -0700

    done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,12 @@
-function makeDarkMode(color){
-    let cakeid = document.getElementById("title");
-    cakeid.style.backgroundColor = "#EF476F";
-}
+let counter = 0;
 
-document.getElementById("title").onclick = makeDarkMode;
+document.getElementById("cakeTitle").onclick = () => {
+    let background = document.getElementById("background")
+    if (counter == 0) {
+        counter = 1;
+        background.style.backgroundColor = "#EF476F";
+    } else {
+        counter = 0;
+        background.style.backgroundColor = "#FFF5ED";
+    }
+}
